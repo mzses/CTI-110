2e5051dbe73c419f940a26c8c14fa135406fe11f
mzses <160041522+mzses@users.noreply.github.com>
2024-02-18 13:59:04 -0500

    Add files via upload

---
+++ P1LAB1_OrnelasMoises.py
@@ -0,0 +1,10 @@
+#Moises Ornelas
+#2/15/2024
+#P1LAB1
+#using input/output
+
+#create a variable to hold first name
+first_name = input("Enter your first name: ")
+
+#print first_name with greeting
+print("Hey", first_name, "welcome to zybooks!")
