f095fbb2d8ea92aac98eac3d06a97dca6669ea58
mzses <160041522+mzses@users.noreply.github.com>
2024-03-18 11:38:44 -0400

    Add files via upload

---
+++ P3HW2_Salary_OrnelasMoises.py
@@ -0,0 +1,48 @@
+#Moises Ornelas
+#3-18-2024
+#P3HW2
+
+'''
+ask for user to enter employee name (input)
+
+ask user to input number of hours worked (int)
+
+ask user to input payrate (float)
+
+make if/else statement to determine if user has worked overtime (<40 hours)
+
+if user has more, calc hours - 40 (this will equal ot_hours)
+
+calc ot_hours * payrate (this is ot_pay)
+
+calc reg_hours (40) * payrate (this is reg_pay)
+
+calc (reg_pay + ot_pay) * payrate this will be grosspay
+
+display employee name:
+
+display hours
+
+display payrate
+
+display ot_hours
+
+display ot_pay
+
+display reg_pay
+
+display grosspay
+'''
+
+employee_name = input("Enter employee's name: ")
+
+hours_worked = input(int("Enter number of hours worked: "))
+
+payrate = input(float("Enter employee's payrate: "))
+
+if hours_worked < 40
+hours_worked - 40 
+      
+      
+          
+
