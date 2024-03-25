1bb494d9e35c7412a55e1a89ace8d37429d09fca
mzses <160041522+mzses@users.noreply.github.com>
2024-03-25 11:48:31 -0400

    Add files via upload

---
+++ P4LAB1_MoisesOrnelas.py
@@ -0,0 +1,28 @@
+#draw square
+import turtle
+win = turtle.Screen()
+t = turtle.Turtle()
+
+#add display options
+t.pensize(4)
+t.pencolor("purple")
+t.shape("turtle")
+
+for item in range(4):
+    #actions to happen
+    t.forward(100)
+    t.left(90)
+
+#draw triangle
+
+#add display options
+t.pensize(4)
+t.pencolor("purple")
+t.shape("turtle")
+
+for item in range(3):
+    #actions to happen
+    t.forward(100)
+    t.left(120)
+
+    
