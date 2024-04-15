fcce01e367ea33c0f7278d8b3903dca0903e972c
mzses <160041522+mzses@users.noreply.github.com>
2024-04-15 11:48:02 -0400

    Add files via upload

--- P5LAB_MoisesOrnelas.py
+++ P5LAB_MoisesOrnelas.py
@@ -65,6 +65,54 @@ def disperse_change(change):
                 print("Pennies")
 
 
+def show_available_items(dictionary):
+    print(f"{'Grocery Item':<25}{'Price'}")
+    print("---------------------------------")
+    for key, value in dictionary.items():
+        print(f"{key:<25}{value:.2f}")
+    print("----------------------------------")     
+        
+def add_items(dictionary):
+    cart = []
+    items = input("Enter an item to add to the cart or type 'end' to stop adding items: ")
+    while items != "end":
+        if items in dictionary.keys():
+            cart.append(items)
+        else:
+            print(f"{items} is not in stock")
+            
+        items = input("Enter an item to add to the cart or type 'end' to stop adding items: ")    
+    return cart
+            
+def get_total(cart, dictionary):
+    total = 0
+    for items in cart:
+        total += dictionary[items]
+    return total    
+
 #main logic starts here
 #call function
-disperse_change(83)
+def main():
+    #create dictionary with items and prices
+    items = {"apples": 3.69, "berries": 4.00, "chocolate": 2.89, "turkey": 6.99, "cheese": 4.00, "pepsi": 7.89, "eggs": 3.50, "bread": 3.00}
+    #call the show_available_items function
+    show_available_items(items)
+
+    #call the add_item function
+    cart = add_items(items)
+    print(cart)
+
+    #display items in cart
+    print()
+    print("the items currently in your cart are: ")
+    for items in cart:
+        print(items)
+
+    #call the get_total function
+    cart_total = get_total(cart, items)
+    print(f"{cart_total:.2f}")
+    
+    disperse_change(83)
+
+#call the main
+main()
